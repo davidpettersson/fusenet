3d89a6993686b4ff7df5241249e9d354ea25fbc3
David Pettersson <david@shebang.nu>
2005-03-10 23:25:42 +0000

    Added Doxygen file and a helper file for documenting stuff like namespaces.

---
+++ fusenet/src/documentation.h
@@ -0,0 +1,25 @@
+#ifndef DOCUMENTATION_H
+#define DOCUMENTATION_H
+
+/**
+ * @file documentation.h
+ *
+ * This file contains extra documentation for Doxygen. It must not be
+ * included in any source file.
+ *
+ * @author David Pettersson <david@shebang.nu>
+ */
+#error This file must not be used in any source file
+
+/**
+ * Namespace defined by Per Holm and friends for the communications
+ * and protocol classes. Most definitions have been made obsolete.
+ */
+namespace client_server { }
+
+/**
+ * Namespace for the Fusenet implementation.
+ */
+namespace fusenet { }
+
+#endif
