9c7d6de61dc1168e6ffb2066835fc2b3e0b6f4d1
david <david@localhost>
2005-03-26 17:34:10 +0000

    Added stubs for the database.

---
+++ fusenet/src/database.cc
@@ -0,0 +1,23 @@
+/**
+ * @file database.cc
+ *
+ * This file contains the mostly empty implementation of the database
+ * class.
+ *
+ * @author David Pettersson <david@shebang.nu>
+ */
+
+#include <iostream>
+
+#include "database.h"
+
+namespace fusenet {
+
+  Database::Database(void) {
+    // Does nothing
+  }
+
+  Database::~Database(void) {
+    // Does nothing
+  }
+}
