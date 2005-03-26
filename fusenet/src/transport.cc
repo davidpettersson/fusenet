daf9009391b351d79d28e359e59d7e71eac8e68e
David Pettersson <david@shebang.nu>
2005-03-26 13:53:46 +0000

    The transport class implementation. Forgot to add it in the last commit.

---
+++ fusenet/src/transport.cc
@@ -0,0 +1,22 @@
+/**
+ * @file transport.cc
+ *
+ * This file contains the mostly empty implementation of the transport class.
+ *
+ * @author David Pettersson <david@shebang.nu>
+ */
+
+#include <iostream>
+
+#include "transport.h"
+
+namespace fusenet {
+
+  Transport::Transport(void) {
+    // Does nothing
+  }
+
+  Transport::~Transport(void) {
+    // Does nothing
+  }
+}
