47bdea031e84a70976c0646c5f79a6977b3bcbb3
David Pettersson <david@shebang.nu>
2005-03-26 17:34:10 +0000

    Added stubs for the database.

---
+++ fusenet/src/database.h
@@ -0,0 +1,39 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+/**
+ * @file database.h
+ *
+ * This file contains the base database class.
+ *
+ * @author David Pettersson <david@shebang.nu>
+ */
+
+#include <iostream>
+#include <stdint.h>
+
+namespace fusenet {
+
+  /**
+   * Base class for all databases. This base class is supposed to be
+   * inherited from by all different kinds of databases, in this
+   * project a memory based and a disk based version.
+   */
+  class Database {
+
+  public:
+
+    /**
+     * Create instance.
+     */
+    Database(void);
+
+    /**
+     * Destroy instance.
+     */
+    virtual ~Database(void);
+  };
+}
+
+#endif
+
